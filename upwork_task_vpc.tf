b243e6987885d1d270cd10f431b370843a75a559
panny0109 <85472652+panny0109@users.noreply.github.com>
2022-07-03 11:37:33 -0600

    Delete upwork_task_vpc.tf

--- upwork_task_vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_network" "vpc_network" {
-  project                 = "skilful-reserve-349011"
-  name                    = "test-vpc"
-  auto_create_subnetworks = true
-  mtu                     = 1460
-}
